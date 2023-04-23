4cf02999c5296830150f5a28587ae3252f6c4203
ECHALAYE <etienne.chalaye@ynov.com>
2023-04-23 12:45:25 +0200

    debut player mouvement plus jump

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Rigidbody2D rb;
+    public float speed = 1f;
+    private bool isJumping = false;
+    private float saveYpos;
+    private float jumpAmount = 10;
     // Update is called once per frame
     void Update()
     {
-        
+
+ 
+        if (Input.GetKey(KeyCode.A))
+            moove(Vector3.left);
+        else if (Input.GetKey(KeyCode.D))
+            moove(Vector3.right);
+        else if (Input.GetKey(KeyCode.Space))
+            jump();
+
+
+    }
+
+    public void moove(Vector3 Direction)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + Direction, speed * Time.deltaTime);
+    }
+
+
+    public void jump()
+    {
+        rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+
     }
 }
